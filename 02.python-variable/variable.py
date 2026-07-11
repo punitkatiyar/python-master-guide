b98684a638671fcfd5847143a0a87d2b6de7a1cc
Punit Katiyar <punit.kat@gmail.com>
2026-07-11 19:50:18 +0530

    Document variable naming conventions and types
    Added comments explaining variable naming conventions and data types.

--- 02.python-variable/variable.py
+++ 02.python-variable/variable.py
@@ -1,1 +1,24 @@
+# Variable : Name : Name convention : 
 
+# data Type : int , float , string , boolen
+
+# data = input("Enter Your Name ");
+
+data = "Hello";
+data = "Welcome";
+# DATA = "Hello";
+
+# mydata="A";
+myData="B";
+my_data="C";
+
+# SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+# my-data="D";
+myData1="E";
+my1Data="G";
+# SyntaxError: invalid decimal literal
+# 1myData="H";
+mydata="I";
+MyData="J";
+
+print(mydata);
